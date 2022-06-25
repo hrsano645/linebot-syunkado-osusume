2d28219119acd434a7752a5175362e4720a3ea4d
hiroshi sano <23502+hrsano645@users.noreply.github.com>
2022-06-25 10:44:03 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,108 @@
+from dataclasses import dataclass
+import json
+import os
+import re
+import sys
+
+import dotenv
+from flask import Flask, abort, request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    AudioSendMessage,
+    ButtonsTemplate,
+    CarouselColumn,
+    CarouselTemplate,
+    ConfirmTemplate,
+    MessageAction,
+    MessageEvent,
+    SourceUser,
+    TemplateSendMessage,
+    TextMessage,
+    TextSendMessage,
+    URIAction,
+)
+
+# TODO: 2022/06/25 linebotのインスタンス作るのと順番後でどちらでもいいか試す
+app = Flask(__name__)
+
+dotenv.load_dotenv()
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
+channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+if channel_secret is None:
+    print("Specify LINE_CHANNEL_SECRET as environment variable.")
+    sys.exit(1)
+if channel_access_token is None:
+    print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+
+@dataclass
+class SyunkadoOkasi:
+    name: str
+    bot_sentence: str
+    season: str
+
+
+# お菓子一覧: https://www.shunkado.co.jp/sweets/
+syunkado_okasi_list = [
+    SyunkadoOkasi("うなぎパイ", "夜のお菓子と言ったら！", "通年"),
+    SyunkadoOkasi("うなぎパイVPOS", "ブランデーが入ってさらに美味しい！", "通年"),
+    SyunkadoOkasi("うなぎサブレ", "隠れた名品！", "通年"),
+]
+
+
+# linebot側のcallbackチェック, Line側とのやり取り用
+@app.route("/callback", methods=["POST"])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers["X-Line-Signature"]
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return "callback OK!"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event):
+    event_text = event.message.text
+
+    match event_text:
+        # debug用
+        case "userinfo":
+            if isinstance(event.source, SourceUser):
+                profile = line_bot_api.get_profile(event.source.user_id)
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    [
+                        TextSendMessage(text="Display name: " + profile.display_name),
+                        TextSendMessage(
+                            text="Status message: " + str(profile.status_message)
+                        ),
+                    ],
+                )
+            else:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="Bot can't use profile API without user ID"),
+                )
+        case _:
+            print("何も当たらなかった場合の最後")
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5500))
+    app.run(host="0.0.0.0", port=port)
